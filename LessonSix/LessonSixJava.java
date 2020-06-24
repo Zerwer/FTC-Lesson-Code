631a0353e4ea1c06eccfa093ee739300711e1191
Zerwer <robobug9@gmail.com>
2020-06-24 19:43:48 -0400

    Lesson six

--- LessonSix/LessonSixJava.java
+++ LessonSix/LessonSixJava.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "OperatorDrive")
-public class OperatorDrive extends OpMode{
+public class OperatorDrive extends OpMode {
 
     private DcMotor left;
     private DcMotor right;
@@ -18,43 +18,45 @@ public class OperatorDrive extends OpMode{
     private double servoPos;
     private boolean oldServoButton;
 
-    @Override
-    public void init() {
-        left = hardwareMap.get(DcMotor.class, "leftMotor");
-        right = hardwareMap.get(DcMotor.class, "rightMotor");
-        arm = hardwareMap.get(DcMotor.class, "armMotor");
-        front = hardwareMap.get(Servo.class, "frontServo");
-        back = hardwareMap.get(Servo.class, "backServo");
-        
-        right.setDirection(DcMotorSimple.Direction.REVERSE);
-        
-        servoPos = 0;
-        oldServoButton = false;
-    }
-    
-    @Override
-    public void loop() {
+	@Override
+	public void init() {
+	    left = hardwareMap.get(DcMotor.class, "leftMotor");
+	    right = hardwareMap.get(DcMotor.class, "rightMotor");
+	    arm = hardwareMap.get(DcMotor.class, "armMotor");
+	    front = hardwareMap.get(Servo.class, "frontServo");
+	    back = hardwareMap.get(Servo.class, "backServo");
+	    
+	    right.setDirection(DcMotorSimple.Direction.REVERSE);
+	    
+	    servoPos = 0;
+	    oldServoButton = false;
+	}
+	
+	@Override
+	public void loop() {
         double speed = gamepad1.right_trigger - gamepad1.left_trigger;
         double turn = gamepad1.left_stick_x;
         double armSpeed = gamepad1.right_stick_y;
-        boolean servoButton = gamepad1.x;
-        
+	    boolean servoButton = gamepad1.x;
+
         left.setPower(speed + turn);
         right.setPower(speed - turn);
-        
+
         arm.setPower(armSpeed);
-        
-        if(servoButton && !oldServoButton) {
-            if(servoPos == 0) {
-                servoPos = 0.5;
-            }
-            else {
-                servoPos = 0;
-            }
-            front.setPosition(servoPos);
-            back.setPosition(servoPos);
-        }
-        
-        oldServoButton = servoButton;
-    }
+
+	    if(servoButton && !oldServoButton) {
+	        if(servoPos == 0) {
+	            front.setPosition(0);
+	            back.setPosition(1);
+	            servoPos = 1;
+	        }
+	        else {
+	            front.setPosition(0.5);
+	            back.setPosition(0.5);
+	            servoPos = 0;
+	        }
+	    }
+	    
+	    oldServoButton = servoButton;
+	}
 }
